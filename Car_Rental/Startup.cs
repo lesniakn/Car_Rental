f1aaec1cac2392e95c9f975f1b8611b4bd7350a1
Norbert Leśniak <norbert.lesniak@gmail.com>
2019-04-18 09:37:07 +0200

    Add project files.

---
+++ Car_Rental/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Car_Rental.Startup))]
+namespace Car_Rental
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
